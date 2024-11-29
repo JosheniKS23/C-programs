ec553d72079581220710f14edb161883ae55282a
JosheniKS23 <josheniks@gmail.com>
2024-11-29 23:37:09 +0530

    Add files via upload

---
+++ area_sphere.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+float= r,ar,vol,pi=3.14f;
+printf("Enter the radius of the sphere:\n");
+scanf("%f",&r);
+ar=4*pi*r*r;
+vol=(4.0f*pi*r*r*r)/3.0f;
+printf("Surface area=%lf\nVolume=%lf\n",ar,vol);
+}
